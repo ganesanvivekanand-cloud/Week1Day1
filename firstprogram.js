70d1f45a58f634a9e9581c207b19c1baa07a82bc
ganesanvivekanand-cloud <ganesanvivekanand@gmail.com>
2026-09-17 23:18:19 +0530

    Add files via upload

---
+++ firstprogram.js
@@ -0,0 +1,30 @@
+console.log("Hello world")
+
+var word='Vivekanand'
+var value
+
+console.log(typeof word)
+
+
+let firstname="Vivekanand"
+console.log(typeof firstname)
+let companyName='ABC'
+console.log(typeof companyName)
+let mobileNumber =9710075631
+console.log(typeof mobileNumber)
+let isAutomation=true
+console.log(typeof isAutomation)
+
+let a =5;
+let b=3;
+
+console.log(Math.floor(a/b))
+
+console.log(a/b)
+
+console.log(a%b)
+
+
+
+
+
