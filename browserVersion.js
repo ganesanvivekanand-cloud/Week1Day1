1edec0dcd5f5559aee124c5a26a3183ef84ba256
ganesanvivekanand-cloud <ganesanvivekanand@gmail.com>
2026-09-18 10:20:40 +0530

    Add files via upload

---
+++ browserVersion.js
@@ -0,0 +1,20 @@
+const browserVersion='Global Variable Chrome'
+
+function getBrowserVersion(){
+
+
+    if(browserVersion=='Global Variable Chrome'){
+
+
+        const browserVersion='Local Variable Chrome'
+        
+        console.log(browserVersion)
+        
+    }
+
+    console.log(browserVersion)
+
+
+}
+
+getBrowserVersion()
